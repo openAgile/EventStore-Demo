6fae68e8954aa65afa9b3b91fe6736f62afa998e
Josh Gough <jsgough@gmail.com>
2014-09-24 13:44:46 -0400

    S-47655 Try cd and invoke by name the V1 installer

--- Get-VersionOneInstaller.ps1
+++ Get-VersionOneInstaller.ps1
@@ -4,4 +4,6 @@ if(-not (Test-Path $tempPath)) { mkdir $tempPath }
 $v1url = "https://s3.amazonaws.com/versionone-chocolatey/VersionOne.Setup-Ultimate-14.3.0.18.cs.exe"
 $installerPath = "$tempPath\VersionOne.Setup-Ultimate-14.3.0.18-cs.exe"
 Invoke-WebRequest $v1url -OutFile $installerPath
-iex "& '$installerPath' -Quiet:2 -LogFile:C:\log\log.log"
+cd $tempPath
+.\VersionOne.Setup-Ultimate-14.3.0.18-cs.exe -Quiet:2 -LogFile:C:\log\log.log
+# iex "& '$installerPath' -Quiet:2 -LogFile:C:\log\log.log"
