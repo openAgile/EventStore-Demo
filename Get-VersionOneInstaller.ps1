f199b953f7469cb0195b97895df00d4b77b2f21b
Josh Gough <jsgough@gmail.com>
2014-09-24 17:40:54 -0400

    S-47665 sqlcmd to reseed db; copy user.config to wwwroot

--- Get-VersionOneInstaller.ps1
+++ Get-VersionOneInstaller.ps1
@@ -1,2 +1,4 @@
 cuninst CommitStreamVersionOne
 cinst CommitStreamVersionOne -source https://www.myget.org/F/versionone/
+sqlcmd -Q "use VersionOne; DBCC CHECKIDENT(NumberSource_Story, RESEED, 47665)"
+copy -force c:\versionone\user.config c:\inetpub\wwwroot\VersionOne
