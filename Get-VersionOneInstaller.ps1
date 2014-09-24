f7ff5ac5622c9b1594a8a9cb5986332c242d87bc
Josh Gough <jsgough@gmail.com>
2014-09-24 15:47:38 -0400

    S-47665 replace script with cinst call to new feature branch package

--- Get-VersionOneInstaller.ps1
+++ Get-VersionOneInstaller.ps1
@@ -1,10 +1,1 @@
-$localPath = "Temp - $($env:computername)"
-$tempPath = "C:\$localPath"
-if(-not (Test-Path $tempPath)) { mkdir $tempPath }
-$v1url = "https://s3.amazonaws.com/versionone-chocolatey/VersionOne.Setup-Ultimate-14.3.0.18.cs.exe"
-$installerPath = "$tempPath\VersionOne.Setup-Ultimate-14.3.0.18-cs.exe"
-Invoke-WebRequest $v1url -OutFile $installerPath
-cd $tempPath
-cinst VersionOne -source https://www.myget.org/F/versionone/
-# .\VersionOne.Setup-Ultimate-14.3.0.18-cs.exe -Quiet:2 -LogFile:C:\log\log.log
-# iex "& '$installerPath' -Quiet:2 -LogFile:C:\log\log.log"
+cinst CommitStreamVersionOne -source https://www.myget.org/F/versionone/
