04b910da013d8a3e8f206552321f0862001aea6f
Josh Gough <jsgough@gmail.com>
2014-09-24 13:35:38 -0400

    S-47665 Try single \ in quoted path

--- Get-VersionOneInstaller.ps1
+++ Get-VersionOneInstaller.ps1
@@ -2,6 +2,6 @@ $localPath = "Temp - $($env:computername)"
 $tempPath = "C:\$localPath"
 if(-not (Test-Path $tempPath)) { mkdir $tempPath }
 $v1url = "https://s3.amazonaws.com/versionone-chocolatey/VersionOne.Setup-Ultimate-14.3.0.18.cs.exe"
-$installerPath = "$tempPath\\VersionOne.Setup-Ultimate-14.3.0.18-cs.exe"
+$installerPath = "$tempPath\VersionOne.Setup-Ultimate-14.3.0.18-cs.exe"
 Invoke-WebRequest $v1url -OutFile $installerPath
 iex "& '$installerPath' -Quiet:2 -LogFile:C:\\log\\log.log"
