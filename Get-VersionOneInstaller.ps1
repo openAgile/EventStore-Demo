46cef4238c43cc887a573f75fecbaf8ecf0f89e4
amitair <amitair@users.noreply.github.com>
2014-09-24 11:30:07 -0400

    updated version

--- Get-VersionOneInstaller.ps1
+++ Get-VersionOneInstaller.ps1
@@ -1,7 +1,7 @@
 $localPath = "Temp - $($env:computername)"
 $tempPath = "C:\$localPath"
 if(-not (Test-Path $tempPath)) { mkdir $tempPath }
-$v1url = "https://s3.amazonaws.com/versionone-chocolatey/VersionOne.Setup-Enterprise-14.3.0.3-cs.exe"
-$installerPath = "$tempPath\\VersionOne.Setup-Enterprise-14.3.0.3-cs.exe"
+$v1url = "https://s3.amazonaws.com/versionone-chocolatey/VersionOne.Setup-Ultimate-14.3.0.13-cs.exe"
+$installerPath = "$tempPath\\VersionOne.Setup-Ultimate-14.3.0.13-cs.exe"
 Invoke-WebRequest $v1url -OutFile $installerPath
 iex "& '$installerPath' -Quiet:2 -LogFile:C:\\log\\log.log"
