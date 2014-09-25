ee8bf024ad4a21d8eb0f6440d430717478f3be26
amitair <amitair@users.noreply.github.com>
2014-09-25 15:24:44 -0400

    s-47665 another try with hardcoded arg

--- Install-V1.ps1
+++ Install-V1.ps1
@@ -8,7 +8,7 @@ iisreset
 
 
 Invoke-WebRequest `
-		    -Uri $args[0] `
+		    -Uri "http://v1commitstream.cloudapp.net/VersionOne/rest-1.v1/Data/Story" `
 		    -Headers @{"Authorization" = "Basic "+[System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes("admin:admin" ))} `
 		    -Method Post `
 		    -Body '<Asset><Attribute name="Name" act="set">Hello world!</Attribute></Asset>'
